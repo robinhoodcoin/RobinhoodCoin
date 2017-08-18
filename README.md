59e81090362ff227d1b5c632a3e970bacb61ae26
Alexander Leitner <leitnersalex@gmail.com>
2017-08-18 12:38:15 -0400

    Steal from the rich

---
+++ README.md
@@ -0,0 +1,7 @@
+# Robinhood Token
+Steal from the rich!
+Get taxed!
+It's how money should be ;)
+
+## TODO
+* Add a tax on all transactions which goes to the richest wallet
---
+++ contracts/RobinhoodCoin.sol
@@ -0,0 +1,120 @@
+pragma solidity ^0.4.11;
+
+import './SafeMath.sol';
+
+/**
+ * @title SimpleToken
+ * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.
+ * Note they can later distribute these tokens as they wish using `transfer` and other
+ * `StandardToken` functions.
+ */
+contract RobinhoodCoin {
+    using SafeMath for uint256;
+
+    event Robbery(address _victim, address _thief, uint256 _amountStolen);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    mapping(address => uint256) balances;
+
+    string public name;
+    string public symbol;
+    uint256 public decimals;
+    uint256 public totalSupply;
+    address public richestDudeAround;
+
+    /* Mining variables */
+    bytes32 public currentChallenge;
+    uint public timeOfLastRobbery; // time of last challenge solved
+    uint256 public difficulty = 2**256 - 1; // Difficulty starts low
+
+    /**
+    * @dev Contructor that gives msg.sender all of existing tokens.
+    */
+    function RobinhoodCoin(
+        string _name,
+        string _symbol,
+        uint256 _decimals,
+        uint256 _totalSupply
+    ) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _totalSupply;
+
+        balances[msg.sender] = totalSupply;
+        richestDudeAround = msg.sender;
+    }
+
+    /**
+     * @dev Calculate the reward
+     * @return uint256 Returns the amount to reward
+     */
+    function calculateAmountToSteal() returns (uint256 reward) {
+        uint256 richMoney = balances[richestDudeAround];
+        reward = richMoney * 1 / 100;
+        if (reward > richMoney) return richMoney;
+
+        return reward;
+    }
+
+    /**
+     * @dev Proof of work to be done for mining
+     * @param nonce uint
+     * @return uint The amount rewarded
+     */
+    function TakeFromTheRich(uint nonce) returns (uint256 reward) {
+        bytes32 n = sha3(nonce, currentChallenge); // generate random hash based on input
+        if (n > bytes32(difficulty)) revert();
+
+        uint timeSinceLastProof = (now - timeOfLastRobbery); // Calculate time since last reward
+        if (timeSinceLastProof < 5 seconds) revert(); // Do not reward too quickly
+
+        reward = calculateAmountToSteal();
+
+        transferFrom(richestDudeAround, msg.sender, reward); // reward to winner grows over time
+
+        if (balances[msg.sender] >= balances[richestDudeAround]) {
+            richestDudeAround = msg.sender;
+        }
+
+        difficulty = difficulty * 10 minutes / timeSinceLastProof + 1; // Adjusts the difficulty
+
+        timeOfLastRobbery = now;
+        currentChallenge = sha3(nonce, currentChallenge, block.blockhash(block.number - 1)); // Save hash for next proof
+
+        Robbery(richestDudeAround, msg.sender, reward); // execute an event reflecting the change
+
+        return reward;
+    }
+
+    /**
+    * @dev Gets the balance of the specified address.
+    * @param _owner The address to query the the balance of.
+    * @return An uint256 representing the amount owned by the passed address.
+    */
+    function balanceOf(address _owner) constant returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) private returns (bool) {
+        if (_to == 0x0) revert();                               // Prevent transfer to 0x0 address. Use burn() instead
+        if (balances[_from] < _value) revert();           // Check if the sender has enough
+        if (balances[_to].add(_value) < balances[_to]) revert(); // Check for overflows
+        balances[_to] = balances[_to].add(_value);
+        balances[_from] = balances[_from].sub(_value);
+        Transfer(_from, _to, _value);
+        return true;
+    }
+
+    /**
+    * @dev transfer token for a specified address
+    * @param _to The address to transfer to.
+    * @param _value The amount to be transferred.
+    */
+    function transfer(address _to, uint256 _value) returns (bool) {
+        transferFrom(msg.sender, _to, _value);
+        return true;
+    }
+
+
+}
